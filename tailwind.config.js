22d83142f16bb8ab72272c2b93a4aa29d6fbfe5c
AbdElrahman3502 <eabd4131@gmail.com>
2025-03-25 05:05:41 +0200

    Update the tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,web,png,ico}"],
+  content: ["./index.html","./src/**/*.{html,js,web,png,ico}"],
   theme: {
     extend: {},
   },
